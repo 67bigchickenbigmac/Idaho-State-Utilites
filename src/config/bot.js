e8b576ed1b88d013971f0a14f0c175147b2a36b6
67bigchickenbigmac <larperlc3@gmail.com>
2026-05-24 17:30:08 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Idaho State Roleplay | Pre Release",
+        name: "Idaho State Roleplay | Pre Release https://discord.gg/dKUSsNzn",
         // Activity type number (0 = Playing).
         type: 0, 
       },
